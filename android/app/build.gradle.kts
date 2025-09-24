4d674e0869321e94f4c054c58d51a999baeccdf3
Abdula-Ibnae-Masud <masud.cse.baue@gmail.com>
2025-09-24 21:34:55 +0600

    changed minSdk and targetSdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.example.popular_gitrepos"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 26 // flutter.minSdkVersion
+        targetSdk = 35 // flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
